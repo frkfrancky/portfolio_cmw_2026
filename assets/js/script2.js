2824fafb789101d76fb61f646bbdba831cef1e93
Santatriniaina <frkfrancky@gmail.com>
2026-02-25 16:00:08 +0100

    add button language

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -3,3 +3,45 @@ if(isDark){
 }else{
     document.body.classList.add("light");
 }
+
+
+let languagedata = {
+    "fr": {
+        "nom": "Rasataharisoa",
+        "prenom": "Francky",
+        "presentation": "Je suis un étudiant en Master 2 CMW",
+    },
+    "en": {
+        "nom": "Rasataharisoa",
+        "prenom": "Francky",
+        "presentation": "I am a Master 2 CMW student",
+    },
+    "kr": {
+        "nom": "Rasataharisoa",
+        "prenom": "Francky",
+        "presentation": "저는 Master 2 CMW 학생입니다",
+    }
+};
+
+// Fonction qui met à jour le texte selon la langue choisie
+function changerLangue(code) {
+    let langue = languagedata[code];
+    Object.keys(langue).forEach(function(cle){
+        document.querySelector("#" + cle).innerHTML = langue[cle];
+    });
+}
+
+changerLangue("fr");
+
+// Boutons
+document.querySelector("#langue-fr").addEventListener("click", function(){
+    changerLangue("fr");
+});
+
+document.querySelector("#langue-en").addEventListener("click", function(){
+    changerLangue("en");
+});
+
+document.querySelector("#langue-kr").addEventListener("click", function(){
+    changerLangue("kr");
+});
